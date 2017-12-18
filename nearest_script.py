c25ad36a22dad332865546c2ab8ab84211d2c198
Amine LALAOUI <alalaoui@student.42.fr>
2017-12-18 15:54:49 +0100

    nearest synonyms of geek script + analogy function what is tokyo to spain what it is to japan, answer madrid

--- nearest_script.py
+++ nearest_script.py
@@ -4,3 +4,25 @@ emb = embeddings.EmbeddingsDictionary(max_words = 100000)
 
 #print(emb.dictionary['geek'])
 print(emb.w2neighbors('geek'))
+
+def word2vec(word):
+    vec = emb.emb[emb.dictionary[word]]
+    return vec
+
+def analog_find(s1, s2, s3):
+    tokyo = word2vec(s1)
+    spain = word2vec(s2)
+    japan = word2vec(s3)
+    madrid = tokyo + spain - japan
+    return (madrid)
+
+def vec2word(vec):
+    word = emb.emb2neighbors(vec, 1)[1][0]
+    return word
+
+def answer(s1, s2, s3):
+    vanswer = vec2word(analog_find(s1,s2, s3))
+    answer = emb.words[vanswer]
+    return answer
+
+print(answer('Tokyo', 'Spain', 'Japan'))
